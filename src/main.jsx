262ce395717f6f5eef3dff2bdfab66c6e65778a1
Siddharta99 <siddhartawakte@gmail.com>
2024-03-20 13:16:56 +0530

    1

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+// import './index.css'
+// import { BrowserRouter } from 'react-router-dom'
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+ 
+//  <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+  
+// )
+
+
+import React from 'react';
+import ReactDOM from 'react-dom'; // Import ReactDOM from 'react-dom' instead of 'react-dom/client'
+import App from './App.jsx';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
- 
- <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  
-)
+  <React.StrictMode>
+    <BrowserRouter> {/* Wrap your App component with BrowserRouter */}
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
